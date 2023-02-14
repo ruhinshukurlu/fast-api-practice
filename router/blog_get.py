ff939488532208964b1385daf0c057d6727ec89f
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-14 23:43:00 +0400

    view params

--- router/blog_get.py
+++ router/blog_get.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from typing import Optional
+from enum import Enum
 
 router = APIRouter(
     prefix='/blog',
@@ -15,3 +16,18 @@ router = APIRouter(
 )
 def get_blogs(page = 1, page_size: Optional[int] = None):
   return {'message': f'All {page_size} blogs on page {page}'}
+
+
+@router.get('/{id}/comments/{comment_id}', tags=['comment'])
+def get_comment(id, comment_id, valid:bool = True, username:Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@router.get('/type/{type}')
+def get_blog_type(type:BlogType):
+    return {'message': f'Blog type {type}'}
