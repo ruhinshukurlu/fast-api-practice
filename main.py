963334b3ef7e629b24206dc867429b194d2ab194
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-16 14:15:06 +0400

    create and read user

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 
 from fastapi import FastAPI
-from router import blog_get
-from router import blog_post
+from router import blog_post, blog_get, user
 from db.database import engine
 from db import models
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
 
 @app.get('/hello')
 def index():
