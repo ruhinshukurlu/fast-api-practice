08ca098d5d4b446f6666af2a5c088fbe3d6f389f
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-18 16:11:07 +0400

    relationship

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
 from fastapi import FastAPI
-from router import blog_post, blog_get, user
+from router import blog_post, blog_get, user, article
 from db.database import engine
 from db import models
 
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
---
+++ router/article.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from schemas import ArticleBase, ArticleDisplay
+from sqlalchemy.orm import Session
+from typing import List
+from db.database import get_db
+from db import db_article
+
+router = APIRouter(
+    prefix='/article',
+    tags=['article']
+)
+
+
+# Create Article
+@router.post('/',response_model=ArticleDisplay)
+def create_article(request:ArticleBase, db:Session = Depends(get_db)):
+    return db_article.create_article(db, request)
+
+# Retrieve article
+@router.get('/{id}',response_model=ArticleDisplay)
+def get_article(id:int, db:Session = Depends(get_db)):
+    return db_article.get_article(db, id)
