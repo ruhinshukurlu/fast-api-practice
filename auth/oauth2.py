e010f38fd90f413e28b6268dda2edd0439e3743f
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-19 10:35:02 +0400

    user authentication

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,8 +1,13 @@
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, status
+from sqlalchemy.orm.session import Session
 from typing import Optional
 from datetime import datetime, timedelta
 from jose import jwt
-
+from jose.exceptions import JWTError
+from fastapi import Depends
+from db.database import get_db
+from db import db_user
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -11,11 +16,35 @@ ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-  to_encode = data.copy()
-  if expires_delta:
-    expire = datetime.utcnow() + expires_delta
-  else:
-    expire = datetime.utcnow() + timedelta(minutes=15)
-  to_encode.update({"exp": expire})
-  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-  return encoded_jwt
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db:Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate":"Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db_user.get_user_by_username(db, username)
+
+    print(user)
+    if user is None:
+        raise credentials_exception
+
+    return user
--- router/user.py
+++ router/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from db.database import get_db
 from db import db_user
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix='/user',
@@ -18,22 +19,22 @@ def create_user(request:UserBase, db: Session = Depends(get_db)):
 
 # Read all users
 @router.get('/all', response_model = List[UserDisplay])
-def get_all_users(db:Session = Depends(get_db)):
+def get_all_users(db:Session = Depends(get_db), current_user:UserBase = Depends(get_current_user)):
     return db_user.get_all_users(db)
 
 
 # Read one user
 @router.get('/{id}', response_model=UserDisplay)
-def get_one_user(id:int, db:Session=Depends(get_db)):
+def get_one_user(id:int, db:Session=Depends(get_db), current_user:UserBase = Depends(get_current_user)):
     return db_user.get_user(id, db)
 
 
 # Update user
 @router.post('/{id}/update')
-def update_user(id:int, request:UserBase, db:Session = Depends(get_db)):
+def update_user(id:int, request:UserBase, db:Session = Depends(get_db), current_user:UserBase = Depends(get_current_user)):
     return db_user.update_user(db, id, request)
 
 
 @router.get('/{id}/delete')
-def delete_user(id:int, db:Session = Depends(get_db)):
+def delete_user(id:int, db:Session = Depends(get_db), current_user:UserBase = Depends(get_current_user)):
     return db_user.delete_user(db, id)
